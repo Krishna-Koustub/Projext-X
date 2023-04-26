76d5815c5cb2177a799430499265a5559d6cd800
krishna koustub <krishnakoustub@gmail.com>
2023-04-26 12:57:27 +0530

    first commit

---
+++ satellite.py
@@ -0,0 +1,102 @@
+import turtle 
+tr = turtle.Turtle()
+rad = 80
+for i in range(2):
+    tr.forward(300)
+    tr.right(90)
+    tr.forward(150)
+    tr.right(90)
+
+for i in range(2):
+    tr.forward(150)
+    tr.right(-45)
+    tr.forward(20)
+    tr.right(-45)
+    tr.forward(150)
+    tr.right(-45)
+    tr.forward(20)
+    tr.right(-45)
+
+for i in range(1):
+    tr.right(90)
+    tr.backward(-0)
+    tr.right(-360)
+    tr.forward(150)
+    tr.right(180)
+    tr.backward(150)
+    tr.right(90)
+    tr.forward(150)
+    tr.left(90)
+    tr.forward(150)
+
+for i in range(1):
+    tr.right(90)
+    tr.forward(150)
+    tr.left(90)
+    tr.forward(75)
+    tr.right(90)
+    
+    tr.forward(40)
+    tr.right(90)
+
+tr.circle(rad)
+  
+for i in range(1):
+    tr.right(90)
+    tr.forward(40)
+    tr.right(90)
+    tr.forward(75)
+    tr.left(90)
+    tr.forward(300)
+
+for i in range(1):
+    tr.left(90)
+    tr.forward(20)
+    tr.left(90)
+    tr.forward(300)
+    tr.right(90)
+    tr.forward(20)
+    tr.right(90)
+    tr.forward(300)
+    tr.left(90)
+    tr.forward(20)
+    tr.left(90)
+    tr.forward(300)
+    tr.right(90)
+    tr.forward(20) 
+    tr.right(90)
+    tr.forward(300)
+    tr.left(90)
+    tr.forward(20)
+    tr.left(90)
+    tr.forward(300)
+    tr.right(90)
+    tr.forward(20)
+    tr.right(90)
+    tr.forward(300)
+    tr.left(90)
+    tr.forward(20)
+    tr.left(90)
+    tr.forward(300)
+    tr.right(90)
+    tr.forward(10)
+    tr.right(90)
+    tr.forward(100)
+    tr.right(90)
+    tr.forward(150)
+    tr.left(90)
+    tr.forward(100)
+    tr.left(90)
+    tr.forward(150)
+    tr.right(90)
+
+    
+
+
+
+
+
+
+
+
+turtle.done()
